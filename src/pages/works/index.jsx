5c5c65445b9c865835b839ef12779068996d251d
muhammadridhoabror <79835369+mridho017@users.noreply.github.com>
2023-01-14 14:13:02 +0700

    bug fix

--- src/pages/works/index.jsx
+++ src/pages/works/index.jsx
@@ -21,14 +21,14 @@ const worksData = [
 
 const Works = () => {
     return (
-        <div className='h-screen p-8 flex md:flex-row flex-col pt-[120px] md:pt-0 scrollbar-hide'>
+        <div className='h-screen p-8 flex md:flex-row flex-col pt-[120px] md:pt-0'>
             <div className='md:w-[30%] md:fixed h-full flex items-center'>
                 <div>
                     <p className='text-white font-glory md:text-5xl text-xl text-cyan md:pb-8'>MY <span className='md:text-7xl text-3xl text-transparent bg-clip-text bg-gradient-to-r from-pink to-blue'>WORKS</span></p>
                     <p className='text-white md:text-2xl text-xs'>Here is a list of projects I've worked on</p>
                 </div>
             </div>
-            <div className='md:pl-[34%] w-[100%] relative md:pt-[110px] pt-8'>
+            <div className='md:pl-[34%] w-[100%] relative md:pt-[110px] pt-8 overflow-auto scrollbar-hide'>
                 <Projects data={worksData} />
             </div>
         </div>
